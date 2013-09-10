a9961254a07191f89ad71cfd2073d76d8d3807ca
thedavejay <thedavejay@gmail.com>
2013-09-10 09:56:59 +0100

    added the projects and solution

---
+++ WebApiTests/Features/HttpFeature.feature
@@ -0,0 +1,19 @@
+﻿Feature: HttpFeature
+	In order to make an http request
+	As a webapi tester
+	I want to be able to call the webapi generically in all my tests
+	
+
+Scenario: Test HTTP call
+	Given I make a new request to localhost on port 9999
+	And the path is api/voicecontinuity
+	And I add the following fields as query parameters
+	| key   | value         |
+	| cli   | +1            |
+	| ddi   | +441623299981 |
+	| stage | initial       |
+	When the request has compeleted
+	Then the status code should be 200
+	
+	
+	
---
+++ WebApiTests/HelperSteps/HttpClientSteps.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using TechTalk.SpecFlow;
+
+namespace WebApiTests.HelperSteps
+{
+  [Binding]
+  public class HttpClientSteps
+  {
+
+    public const string CURRENTURI = "HttpClientSteps_CurrentUri";
+    public const string CURRENTRESPONSE = "HttpClientSteps_CurrentResponse";
+
+    [Given(@"I make a new request to (.*) on port (\d+)")]
+    public void IMakeANewHttpRequestToHostOnPort(string host, int port)
+    {
+      UriBuilder builder = new UriBuilder();
+      builder.Host = host;
+      builder.Port = port;
+      ScenarioContext.Current.Add(CURRENTURI, builder);
+      ScenarioContext.Current.Remove(CURRENTRESPONSE);
+    }
+
+    [Given(@"the path is (.*)")]
+    public void ThePathIs(string path)
+    {
+      UriBuilder builder = ScenarioContext.Current.Get<UriBuilder>(CURRENTURI);
+      builder.Path = path;
+    }
+
+    [Given(@"I add the following fields as query parameters")]
+    public void IAddTheFollowingFieldsAsQueryParameters(Table fields)
+    {
+      UriBuilder builder = ScenarioContext.Current.Get<UriBuilder>(CURRENTURI);
+      StringBuilder sBuild = new StringBuilder();
+      foreach (var row in fields.Rows)
+      {
+        sBuild.Append(string.Format("{0}={1}&", HttpUtility.UrlEncode(row["key"]), HttpUtility.UrlEncode(row["value"])));
+      }
+      builder.Query += sBuild;
+    }
+
+    [When(@"the request has compeleted")]
+    public void TheRequestHasCompleted()
+    {
+      UriBuilder builder = ScenarioContext.Current.Get<UriBuilder>(CURRENTURI);
+      using (HttpClient client = new HttpClient())
+      {
+        HttpResponseMessage response = client.GetAsync(builder.Uri).Result;
+        ScenarioContext.Current.Add(CURRENTRESPONSE, response);
+      }
+    }
+
+    [Then(@"the status code should be (\d+)")]
+    public void TheStatusCodeShouldBe(int statusCode)
+    {
+      var response = ScenarioContext.Current.Get<HttpResponseMessage>(CURRENTRESPONSE);
+
+      Assert.Equal(statusCode, (int)response.StatusCode);
+    }
+
+
+  }
+}
